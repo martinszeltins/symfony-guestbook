5fa0723016ff7450488fbef9c471c0f38e2dac88
Martins Zeltins <martins.zeltins@arkbauer.com>
2021-06-19 15:50:50 +0300

    Right before starting Step 13: Managing the Lifecycle of Doctrine Objects

--- app/src/Controller/ConferenceController.php
+++ app/src/Controller/ConferenceController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,13 +13,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ConferenceController extends AbstractController
 {
-    /**
-     * @Route("/hello/{name}", name="homepage", methods={"GET"})
-     */
-    public function index($name = ''): Response
-    {
-        return new Response("Hello, $name");
+    public function __construct(private CommentRepository $commentRepository) {}
 
+    #[Route('/', name: 'homepage')]
+    public function index(ConferenceRepository $conferenceRepository): Response
+    {
         return $this->render('conference/index.html.twig');
+    }
+
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(Request $request, Conference $conference): Response
+    {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $this->commentRepository->getCommentPaginator($conference, $offset);
+
+        return $this->render('conference/show.html.twig', [
+            'conference'    => $conference,
+            'comments'      => $paginator,
+            'next'          => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'previous'      => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+        ]);
     }
 }
